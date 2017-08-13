2182b75c7d39617682cab8a4a238deae4843e674
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-08-13 02:20:51 -0700

    Completed the assignment but need to research about datetime

--- apps/timedisplay/views.py
+++ apps/timedisplay/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from time import gmtime, strftime
+import datetime
 
-# Create your views here.
+def index(request):
+#   context = {
+#   "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+#   }
+    context = {
+    "time": datetime.datetime.now()
+    }
+    return render(request,'timedisplay/time.html', context)
